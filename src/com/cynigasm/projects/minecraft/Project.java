79079db7cbaf48971804592d9e925643d2963b57
Cynigasm <taurinestemple@gmail.com>
2017-06-01 04:44:48 +0100

    Base file for the project.

---
+++ src/com/cynigasm/projects/minecraft/Project.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * Class Notes Section
+ *
+ */
+
+package com.cynigasm.projects.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Cynigasm 
+ * @website https://trello.com/b/Yx15qVRx
+ * 1 Jun 2017 
+ */
+
+public class Project extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
